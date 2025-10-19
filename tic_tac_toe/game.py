7adf9e1ccef6afb86b165f5787553b94b9acfdd5
Фёдор Баскаков <feodor.bask@yandex.ru>
2025-10-19 19:59:57 +0300

    finally

--- tic_tac_toe/game.py
+++ tic_tac_toe/game.py
@@ -2,18 +2,14 @@ from gameparts import Board
 from gameparts.exceptions import CellOccupiedError, FieldIndexError
 def main():
     game = Board()
-    # Первыми ходят крестики.
     current_player = 'X'
-    # Это флаговая переменная. По умолчанию игра запущена и продолжается.
     running = True
     game.display()
 
-    # Тут запускается основной цикл игры.
     while running:
 
         print(f'Ход делают {current_player}')
 
-        # Запускается бесконечный цикл.
         while True:
             try:
                 row = int(input('Введите номер строки: '))
@@ -22,6 +18,8 @@ def main():
                 column = int(input('Введите номер столбца: '))
                 if column < 0 or column >= game.field_size:
                     raise FieldIndexError
+                if game.board[row][column] != ' ':
+                    raise CellOccupiedError
             except FieldIndexError:
                 print(
                     'Значение должно быть неотрицательным и меньше '
@@ -30,8 +28,8 @@ def main():
                 print('Введите значения для строки и столбца заново.')
                 continue
             except CellOccupiedError:
-                print('Ячейка занята')
-                print('Введите другие координаты.')
+                print('Ячейка занята.')
+                print('Пожалуйста, введите другие координаты.')
                 continue
             except ValueError:
                 print('Буквы вводить нельзя. Только числа.')
@@ -42,13 +40,17 @@ def main():
             else:
                 break
 
-        # Теперь для установки значения на поле само значение берётся
-        # из переменной current_player.
         game.make_move(row, column, current_player)
         game.display()
-        # Тернарный оператор, через который реализована смена игроков.
-        # Если current_player равен X, то новым значением будет O,
-        # иначе — новым значением будет X.
+        # После каждого хода надо делать проверку на победу и на ничью.
+        if game.check_win(current_player):
+            print(f'Победили {current_player}!')
+            running = False
+        elif game.is_board_full():
+            print('Ничья!')
+            running = False
+
         current_player = 'O' if current_player == 'X' else 'X'
+
 if __name__=='__main__':
     main()
